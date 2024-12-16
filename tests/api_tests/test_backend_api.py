9f7e8bedd7ca31237e4b58f67ea02c0eff8c083f
StixSimfi <stix_simfi@mail.ru>
2024-12-16 16:56:26 +0300

    Update test_backend_api.py

--- tests/api_tests/test_backend_api.py
+++ tests/api_tests/test_backend_api.py
@@ -12,11 +12,10 @@ import pytest
 @pytest.mark.usefixtures('create_configurations')
 class TestBackendDockerManagerApi:
     """
-
+    Тесты проверки API основного функционала приложения
     """
-
-    @allure.story('')
-    @allure.feature('')
+    @allure.feature('Команды управления контейнерами')
+    @allure.story('Проверяем команды управления контейнерам: start, stop, restart')
     @pytest.mark.api
     @pytest.mark.parametrize(
         "command, container_name",
@@ -30,21 +29,25 @@ class TestBackendDockerManagerApi:
         ]
     )
     def test_start_command_buttons(self, command, container_name):
+        answer: str = ""
         resp = self._http_client.get(f"/api/docker-qa-manage/{command}/{container_name}")
         self._logger.debug(resp.status_code)
         self._logger.debug(resp.text)
-        answer = json.loads(resp.text)
+        with allure.step("Команда отправлеена получаем данные из ответа"):
+            answer = json.loads(resp.text)
         assert answer["command"] == command
         assert answer["status"] == "success"
-        assert resp.status_code == 200
+        assert resp.status_code == 200, f"Статус код {resp.status_code} не соответствует ожидаемому значению"
 
-    @allure.story('')
-    @allure.feature('')
+    @allure.feature('Команда получения списка контейнеров')
+    @allure.story('Проверяем список контейнеров')
     @pytest.mark.api
     def test_getting_container_list(self):
+        answer: str = ""
         resp = self._http_client.get("/api/docker-qa-manage/ps")
         self._logger.debug(resp.status_code)
         self._logger.debug(resp.text)
-        answer = json.loads(resp.text)
+        with allure.step("Команда отправлеена получаем данные из ответа"):
+            answer = json.loads(resp.text)
         assert answer is not None
-        assert resp.status_code == 200
+        assert resp.status_code == 200, f"Статус код {resp.status_code} не соответствует ожидаемому значению"
